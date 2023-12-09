4b54c99b491776d493589a4990e5546cf6f9e9b4
Sezen Çetinkaya <152431280+SezenCetinkaya@users.noreply.github.com>
2023-12-09 18:24:43 +0300

    Functional Requirement 1
    There is some updates for "+" sign for cards which are from game deck.

--- GameDeck.java
+++ GameDeck.java
@@ -1,61 +1,41 @@
-import java.util.Random;
-
 public class GameDeck{
 
-private char[] colour;
-private char[] number;
+private String[] colour;
+private String[] number;
 private String[] deck;
 
-
 public GameDeck(){
-    colour= new char[]{'B','R','G','Y'};
-	number= new char[10];
-	
-}
+    colour= new String[]{"B","R","G","Y"};
+	number= new String[]{"1","2","3","4","5","6","7","8","9","10"};
+	deck=new String[40];
+	}
 
 public String[] createDeck(){
-	String[] deck=new String[40];
 	int row=0;
 	String card="";
-	int k=1;
-		
-	for (char c : colour) {
-        for (char n : number) {
-            card = k + String.valueOf(c);
+	
+	for (String c : colour) {
+        for (String n : number) {
+            card = '+'+String.valueOf(n)+ String.valueOf(c);
             deck[row] = card;
             row++;
-            k++;
-        }k=1;	
+        }	
 	}
-		String out = "";  
-		String out1= "";
-		String out2= "";
-		String out3= "";
-		for (String card1 : deck) {
-		    if(card1.charAt(1)=='B'){
-				 out += card1 + "-";
-			}
-			if(card1.charAt(1)=='R'){
-				 out1 += card1 + "-";
-			}
-			if(card1.charAt(1)=='G'){
-				 out2 += card1 + "-";
-			}
-			if(card1.charAt(1)=='Y'){
-				 out3 += card1 + "-";
-			}
-		}
-		System.out.print("These are Blue cards  :");
-		System.out.println(out+"10B");
-		System.out.print("These are Red cards   :");
-		System.out.println(out1+"10R");
-		System.out.print("These are Green cards :");
-		System.out.println(out2+"10G");
-		System.out.print("These are Yellow cards:");
-		System.out.println(out3+"10Y");
-		
 	
-		return deck;
+	for (String c : colour) {
+        String out = "";
+        for (String card1 : deck) {
+            if (card1.charAt(2)==c.charAt(0)) {
+                out += card1 + "/";
+			}
+			if(card1.charAt(2)=='0' && card1.charAt(3)==c.charAt(0)){
+				out += card1 ;
+			}
+        }
+        System.out.println("These are " + c + " cards: " + out);
+    }
+	
+	return deck;
 }
 }
 

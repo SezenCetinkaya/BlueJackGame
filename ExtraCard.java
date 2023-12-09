5824b56b5fed1fd32041ffbd3e611821d2fbe2cb
Sezen Çetinkaya <152431280+SezenCetinkaya@users.noreply.github.com>
2023-12-09 20:01:57 +0300

    Functional Requirement 3
    The remaining five cards are randomly
    generated. Three cards with random colors and values between 1 to 6, with an additional sign, that is either a plus (+) or a minus (-). The remaining two cards have an 80 percent chance of being a signed card. One card can be a flip (+/-) card. One card can be a double (x2) card. Flip cards and double cards do not have a color, they only change the values or
    the sign of the last played card.

---
+++ ExtraCard.java
@@ -0,0 +1,128 @@
+import java.util.Random;
+
+public class ExtraCard{
+
+private String[] userDeck;
+private String[] computerDeck;
+private String[] playCards;
+private char[] colours;
+private String[] signedCardU;
+private String[] signedCardC;
+
+public ExtraCard(){
+	colours= new char[]{'B','R','G','Y'};
+	userDeck=new String[10];
+	computerDeck=new String[10];
+	Spread spread=new Spread();
+	playCards=spread.spreadCards();
+	GetSignedCard getSignedCard = new GetSignedCard();
+	signedCardU=getSignedCard.getSignedCard();
+	signedCardC=getSignedCard.getSignedCard();
+}
+
+private int plus(){
+	Random r= new Random();
+	int percentage1=r.nextInt(5);
+	int percentage2=r.nextInt(5);
+	if(percentage1==0&&percentage2==0){
+		return 0;
+	}
+	else if(percentage1==0 || percentage2==0){
+		return 1;
+	}
+	else{
+		return 2;
+	}
+}
+public String[] extraCardUser(){
+	Random r= new Random();
+	int sign=0;
+	int number=0;
+	int colour=0;
+	int plus=plus();
+	
+	for(int i=0;i<5;++i){
+		userDeck[i]=playCards[i];
+	}
+	
+	for(int j=5;j<8+plus;++j){
+		
+		sign=r.nextInt(2);
+		if(sign==0){
+			userDeck[j]="+";
+		}else{
+			userDeck[j]="-";
+		}
+		
+		number=r.nextInt(6)+1;
+		userDeck[j]+=number;
+	
+		colour=r.nextInt(4);
+		userDeck[j]+=colours[colour];
+	}
+	switch(plus){
+		case 0:
+			userDeck[8]=signedCardU[0];
+			userDeck[9]=signedCardU[1];
+		case 1 :
+			userDeck[9]=signedCardU[0];
+		default:
+			break;
+	}
+	
+	/*for(int i=0;i<10;++i){
+		System.out.println(userDeck[i]);
+	}
+	*/
+	return userDeck;
+	}
+	
+public String[] extraCardComputer(){
+	Random r= new Random();
+	int sign=0;
+	int number=0;
+	int colour=0;
+	int row =0;
+	int plus=plus();
+	
+	for(int i=5;i<10;++i){
+		computerDeck[row]=playCards[i];
+		row++;
+	}
+	
+	for(int j=5;j<8+plus;++j){
+		
+		sign=r.nextInt(2);
+		if(sign==0){
+			computerDeck[j]="+";
+		}else{
+			computerDeck[j]="-";
+		}
+		
+		number=r.nextInt(6)+1;
+		computerDeck[j]+=number;
+	
+		colour=r.nextInt(4);
+		computerDeck[j]+=colours[colour];
+	}
+	
+	switch(plus){
+		case 0:
+			computerDeck[8]=signedCardC[0];
+			computerDeck[9]=signedCardC[1];
+		case 1 :
+			computerDeck[9]=signedCardC[0];
+		default:
+			break;
+	}
+	
+	/*for(int i=0;i<10;++i){
+		System.out.println(computerDeck[i]);
+	}
+	*/
+	return computerDeck;
+}
+
+}
+
+     

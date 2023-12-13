baf1e2f5ad864b1e90f88414efc5c504fc74c20e
Sezen Çetinkaya <152431280+SezenCetinkaya@users.noreply.github.com>
2023-12-13 12:05:20 +0300

    Functional Requirement 4
    The program is able to move cards from the game deck to the players and the boards.

--- Test.java
+++ Test.java
@@ -12,9 +12,9 @@ public class Test{
         enter.enter(); 
 		
 		ShuffleCards shuffleCards = new ShuffleCards();
-		String[] shuffledDeck=shuffleCards.shuffleDeck();
+		String[] shuffledDeck1=shuffleCards.shuffleDeck();
 		
-		ExtraCard extraCard= new ExtraCard(shuffledDeck);
+		ExtraCard extraCard= new ExtraCard(shuffledDeck1);
 		String[] userDeck= extraCard.extraCardUser();
 		String[] computerDeck=extraCard.extraCardComputer();
 		
@@ -33,7 +33,7 @@ public class Test{
 		}
 		
 		System.out.println("");
-		System.out.println("");
+		System.out.println(" ");
 		
 		System.out.println("Your turn to take cards!");
 		
@@ -71,9 +71,141 @@ public class Test{
 		}
 		System.out.println("");
 		
-		Game game=new Game(computerPlayDeck,userPlayDeck,shuffledDeck);
+		//Game game=new Game(computerPlayDeck,userPlayDeck,shuffledDeck);
 		
-		game.game();
+		//game.game();
+		
+		String[] computerBoard=new String[9];
+	    String[] userBoard=new String[9];
+		
+		String[] shuffledDeck=new String[30];
+		
+		for(int i=0;i<30;++i){
+			shuffledDeck[i]=shuffledDeck1[i+5];
+		}
+		
+		for (int i = 0; i < 40; ++i) {
+            System.out.println(shuffledDeck1[i]);
+        }
+		int counter=0;
+		int counter1=0;
+		int sumComputer=0;
+		int sumUser=0;
+		
+		boolean controler1=true;
+		int x;
+		String string="";
+		String string1="";
+		int count;
+		do{
+			computerBoard[counter1]=shuffledDeck[counter];
+			if(!("+/-".equals(computerBoard[counter1])) || !("x2".equals(computerBoard[counter1]))){
+				if (computerBoard[counter1] != null){
+				string=computerBoard[counter1].substring(1, computerBoard[counter1].length()-1);
+				sumComputer=sumComputer+ Integer.parseInt(string);
+				}
+			}
+			counter++;
+			x=computerPlayDeck.length;
+			System.out.print("Computer Hand :");
+			for(int i=0;i<x;++i){
+				System.out.print("x ");
+			}
+			System.out.println("");
+			System.out.print("Computer Board:");
+			controler1=true;
+			count=0;
+			while(controler1){
+				System.out.print(computerBoard[count]+" ");
+				count++;
+				if(computerBoard[count]==null){
+					controler1=false;
+				}
+			}
+			controler1=true;
+			count=0;
+			System.out.println("");
+			System.out.print("Player Board  :");
+			while(controler1){
+				System.out.print(userBoard[count]+" ");
+				count++;
+				if(userBoard[count]==null){
+					controler1=false;
+				}
+			}
+			controler1=true;
+			count=0;
+			System.out.println("");
+			System.out.print("Player Hand   :");
+			while(controler1){
+				System.out.print(userPlayDeck[count]+" ");
+				count++;
+				if(count==4){
+					controler1=false;
+				}
+			}
+			System.out.println("");
+			System.out.println("");
+			
+			userBoard[counter1]=shuffledDeck[counter];
+			
+			if(!"+/-".equals(userBoard[counter1]) || !"x2".equals(userBoard[counter1])){
+				if (userBoard[counter1] != null){
+				string1=userBoard[counter1].substring(1, userBoard[counter1].length()-1);
+				sumUser+=Integer.parseInt(string1);
+				}
+			}
+			counter++;
+			counter1++;
+			x=0;
+			for(int i=0;i<4;++i){
+				if(computerPlayDeck[i]!=null){
+					x++;
+				}
+			}
+			System.out.print("Computer Hand :");
+			for(int i=0;i<x;++i){
+				System.out.print("x ");
+			}
+			System.out.println("");
+			System.out.print("Computer Board:");
+			count=0;
+			controler1=true;
+			while(controler1){
+				System.out.print(computerBoard[count]+" ");
+				count++;
+				if(computerBoard[count]==null){
+					controler1=false;
+				}
+			}
+			controler1=true;
+			count=0;
+			System.out.println("");
+			System.out.print("Player Board  :");
+			while(controler1){
+				System.out.print(userBoard[count]+" ");
+				count++;
+				if(userBoard[count]==null){
+					controler1=false;
+				}
+			}
+			controler1=true;
+			count=0;
+			System.out.println("");
+			System.out.print("Player Hand   :");
+			while(controler1){
+				System.out.print(userPlayDeck[count]+" ");
+				count++;
+				if(count==4){
+					controler1=false;
+			}
+			}
+			System.out.println("");
+			System.out.println("");
+			System.out.println("1:"+sumComputer);
+			System.out.println("1:"+sumUser);
+			
+		}while(sumComputer<20 && sumUser<20);
+    }
 		
 	}
-}
